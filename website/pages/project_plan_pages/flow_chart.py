d92563a38818697aa95b9f6be1b6076c3f965bd3
Niket Jain <niket.jain@accolitedigital.com>
2024-09-23 02:31:56 +0530

    updated path logic

--- website/pages/project_plan_pages/flow_chart.py
+++ website/pages/project_plan_pages/flow_chart.py
@@ -3,4 +3,4 @@ import os
 
 dir_path = os.path.dirname(os.path.realpath(__name__))
 st.header("Project Plan Flow Chart")
-st.image(dir_path + "/data/Project Plan.jpg")
+st.image(r"../data/Project Plan.jpg")
--- website/pages/team_information.py
+++ website/pages/team_information.py
@@ -5,5 +5,5 @@ import os
 dir_path = os.path.dirname(os.path.realpath(__name__))
 
 st.title("Team Member Details")
-df = pd.read_csv(dir_path + "/data/team_details.csv")
+df = pd.read_csv(r"../data/team_details.csv")
 st.table(df)
